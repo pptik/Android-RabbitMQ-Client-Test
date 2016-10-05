a9d05269fe7b63d087f145e6ea7a6217743e8846
Hendra Permana <hynra@yahoo.com>
2016-10-05 19:04:37 +0700

    Implement chat app

--- app/src/main/java/pptik/org/rabbitmqclienttest/MainActivity.java
+++ app/src/main/java/pptik/org/rabbitmqclienttest/MainActivity.java
@@ -1,18 +1,39 @@
 package pptik.org.rabbitmqclienttest;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Consumer;
+import com.rabbitmq.client.DefaultConsumer;
+import com.rabbitmq.client.Envelope;
+
+import java.io.IOException;
 
 import pptik.org.rabbitmqclienttest.rabbit.ManagerRabbitMQ;
 
 public class MainActivity extends AppCompatActivity {
     ManagerRabbitMQ manage;
+    private static final String EXCHANGE_NAME = "sabuga.chat";
+    private static String TAG_INCOMING = "Incoming Message";
+    private String TAG = getClass().getSimpleName();
+    private EditText boxChat;
+    private ImageButton btnSend;
+    private TextView chatMain;
+    private static final String ACTION_STRING_ACTIVITY = "broadcast_event";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,36 +42,59 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        manage = new ManagerRabbitMQ(MainActivity.this);
+        boxChat = (EditText)findViewById(R.id.boxChat);
+        btnSend = (ImageButton)findViewById(R.id.btnsend);
+        chatMain = (TextView)findViewById(R.id.chatMain);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        manage = new ManagerRabbitMQ(MainActivity.this);
+        manage.connectToRabbitMQ();
+        btnSend.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                manage.connectToRabbitMQ();
+            public void onClick(View v) {
+                manage.sendMessage(boxChat.getText().toString());
             }
         });
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
+
+
+
+    @Override
+    protected void onPause() {
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(
+                mMessageReceiver);
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        LocalBroadcastManager.getInstance(this).registerReceiver(
+                mMessageReceiver, new IntentFilter(ACTION_STRING_ACTIVITY));
+        super.onResume();
+    }
+
+    private BroadcastReceiver mMessageReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+          String message = intent.getStringExtra("message");
+            chatMain.setText(chatMain.getText().toString()+"\n"+message);
+        }
+    };
 }
--- app/src/main/java/pptik/org/rabbitmqclienttest/rabbit/ManagerRabbitMQ.java
+++ app/src/main/java/pptik/org/rabbitmqclienttest/rabbit/ManagerRabbitMQ.java
@@ -2,7 +2,9 @@ package pptik.org.rabbitmqclienttest.rabbit;
 
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.Intent;
 import android.os.AsyncTask;
+import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 
 import com.google.gson.Gson;
@@ -29,11 +31,12 @@ public class ManagerRabbitMQ {
 
     protected Channel mChannel = null;
     protected Connection mConnection;
-    private static final String EXCHANGE_NAME = "sabuga.exchange";
+    private static final String EXCHANGE_NAME = "sabuga.chat";
+    private static final String ACTION_STRING_ACTIVITY = "broadcast_event";
 
 
     String userName = "Sabuga";
-    String password = "berabbit";
+    String password = "";
     String virtualHost = "/SabugaRangers";
     String serverIp = "167.205.7.229";
     int port = 5672;
@@ -64,8 +67,6 @@ public class ManagerRabbitMQ {
 
     public void connectToRabbitMQ() {
 
-        final ProgressDialog dialog = new ProgressDialog(context);
-        dialog.show();
 
         if (mChannel != null && mChannel.isOpen()){//already declared
             running = true;
@@ -88,96 +89,11 @@ public class ManagerRabbitMQ {
 
                     mConnection = connectionFactory.newConnection();
                     mChannel = mConnection.createChannel();
-                    Log.i("Tag", "hahaha");
-                    dialog.dismiss();
-               //     registerChanelHost();
-                //    registerChanelListHost();
-                    String msg = "{\n" +
-                            "  \"status\": {\n" +
-                            "    \"code\": 200,\n" +
-                            "    \"success\": true,\n" +
-                            "    \"msg\": \"Data ditemukan\"\n" +
-                            "  },\n" +
-                            "  \"data\": [\n" +
-                            "    {\n" +
-                            "      \"_id\": \"57f4910151a74e1dddc2c6f9\",\n" +
-                            "      \"name\": \"Driver 1\",\n" +
-                            "      \"email\": \"driver1@gmail.com\",\n" +
-                            "      \"nomor_telepon\": \"081311415274\",\n" +
-                            "      \"user\": \"driver1\",\n" +
-                            "      \"pass\": \"U9tJ00U5t68be71e76f206ed3f2b6c37320c57faf0\",\n" +
-                            "      \"role\": \"2\",\n" +
-                            "      \"id_unit\": \"001\",\n" +
-                            "      \"latitude\": \"-6.881694\",\n" +
-                            "      \"longitude\": \"107.615820\",\n" +
-                            "      \"location\": \"Jl. Ir H djuanda, Dago-Coblong, Bandung\",\n" +
-                            "      \"date\": \"October 5th 2016, 12:34:57 pm\",\n" +
-                            "      \"isVerified\": false\n" +
-                            "    },\n" +
-                            "    {\n" +
-                            "      \"_id\": \"57f4911b51a74e1dddc2c6fa\",\n" +
-                            "      \"name\": \"Driver 2\",\n" +
-                            "      \"email\": \"driver2@gmail.com\",\n" +
-                            "      \"nomor_telepon\": \"081311415274\",\n" +
-                            "      \"user\": \"driver2\",\n" +
-                            "      \"pass\": \"NsBdrxReWn27d48576d6d46fd12612c633428d2c2f\",\n" +
-                            "      \"role\": \"2\",\n" +
-                            "      \"id_unit\": \"002\",\n" +
-                            "      \"latitude\": \"-6.877911\",\n" +
-                            "      \"longitude\": \"107.616890\",\n" +
-                            "      \"location\": \"Jl. Ir H djuanda no 399, Dago-Coblong, Bandung\",\n" +
-                            "      \"date\": \"October 5th 2016, 12:35:23 pm\",\n" +
-                            "      \"isVerified\": false\n" +
-                            "    },\n" +
-                            "    {\n" +
-                            "      \"_id\": \"57f4912d51a74e1dddc2c6fb\",\n" +
-                            "      \"name\": \"Driver 3\",\n" +
-                            "      \"email\": \"driver3@gmail.com\",\n" +
-                            "      \"nomor_telepon\": \"081311415274\",\n" +
-                            "      \"user\": \"driver3\",\n" +
-                            "      \"pass\": \"fe89U7PPcO3b611199a29d13442fe49bd4f5a9776b\",\n" +
-                            "      \"role\": \"2\",\n" +
-                            "      \"id_unit\": \"003\",\n" +
-                            "      \"latitude\": \"-6.884797\",\n" +
-                            "      \"longitude\": \"107.613836\",\n" +
-                            "      \"location\": \"Jl. Ir H djuanda no 319, Dago-Coblong, Bandung\",\n" +
-                            "      \"date\": \"October 5th 2016, 12:35:41 pm\",\n" +
-                            "      \"isVerified\": false\n" +
-                            "    },\n" +
-                            "    {\n" +
-                            "      \"_id\": \"57f4913d51a74e1dddc2c6fc\",\n" +
-                            "      \"name\": \"Driver 4\",\n" +
-                            "      \"email\": \"driver4@gmail.com\",\n" +
-                            "      \"nomor_telepon\": \"081311415274\",\n" +
-                            "      \"user\": \"driver4\",\n" +
-                            "      \"pass\": \"QlRw1ag44N8c0261f33dd511989e83ad419f474a6b\",\n" +
-                            "      \"role\": \"2\",\n" +
-                            "      \"id_unit\": \"004\",\n" +
-                            "      \"latitude\": \"-6.889912\",\n" +
-                            "      \"longitude\": \"107.613394\",\n" +
-                            "      \"location\": \"Jl. Ir H djuanda no 124, Dago-Coblong, Bandung\",\n" +
-                            "      \"date\": \"October 5th 2016, 12:35:57 pm\",\n" +
-                            "      \"isVerified\": false\n" +
-                            "    },\n" +
-                            "    {\n" +
-                            "      \"_id\": \"57f4915851a74e1dddc2c6fd\",\n" +
-                            "      \"name\": \"Driver 5\",\n" +
-                            "      \"email\": \"driver5@gmail.com\",\n" +
-                            "      \"nomor_telepon\": \"081311415274\",\n" +
-                            "      \"user\": \"driver5\",\n" +
-                            "      \"pass\": \"cvI0cuNk4xe8274d3ca7c29f1086a7b9d2a44d5421\",\n" +
-                            "      \"role\": \"2\",\n" +
-                            "      \"id_unit\": \"005\",\n" +
-                            "      \"latitude\": \"-6.893624\",\n" +
-                            "      \"longitude\": \"107.613008\",\n" +
-                            "      \"location\": \"Jl. Ir H djuanda no 324, Dago-Coblong, Bandung\",\n" +
-                            "      \"date\": \"October 5th 2016, 2:03:46 pm\",\n" +
-                            "      \"isVerified\": false\n" +
-                            "    }\n" +
-                            "  ]\n" +
-                            "}";
-                    byte[] messageBodyBytes = msg.getBytes();
-                    mChannel.basicPublish(EXCHANGE_NAME, "sabuga.#", null, messageBodyBytes);
+                    Log.i("Connect To host", "connected");
+            //        dialog.dismiss();
+                    registerChanelHost();
+
+
 
                     return true;
 
@@ -192,6 +108,7 @@ public class ManagerRabbitMQ {
                 super.onPostExecute(aBoolean);
                 running = aBoolean;
             }
+
 
         }.execute();
     }
@@ -236,24 +153,19 @@ public class ManagerRabbitMQ {
 
         try{
 
-            mChannel.exchangeDeclare(EXCHANGE_NAME, "topic", true);
+            mChannel.exchangeDeclare(EXCHANGE_NAME, "fanout", true);
 
-            String queueName = mChannel.queueDeclare().getQueue();
-            mChannel.queueBind(queueName, EXCHANGE_NAME, "sabuga.#");
+            final String queueName = mChannel.queueDeclare().getQueue();
+            mChannel.queueBind(queueName, EXCHANGE_NAME, "");
 
             Consumer consumer = new DefaultConsumer(mChannel) {
                 @Override
                 public void handleDelivery(String consumerTag, Envelope envelope,
                                            AMQP.BasicProperties properties, byte[] body) throws IOException {
 
-                    //Verify if device that send the info is different of the are receiver
-               //     getHeader(properties);
-
                     String message = new String(body, "UTF-8");
+                    sendBroadcast(message);
 
-  //                  Gson gson = new Gson();
-//                    Message message1 = gson.fromJson(message, Message.class);
-                    Log.i("mah", message);
 
                 }
             };
@@ -265,11 +177,26 @@ public class ManagerRabbitMQ {
         }
     }
 
+    private void sendBroadcast(String msg) {
+        Intent intent = new Intent(ACTION_STRING_ACTIVITY);
+        intent.putExtra("message", msg);
+        LocalBroadcastManager.getInstance(context).sendBroadcast(intent);
+    }
+
     private  void getHeader(AMQP.BasicProperties properties){
 
         Map<String, Object> headers = properties.getHeaders();
 
         Object deviceId =  headers.get("extraContent");
 
+    }
+
+    public void sendMessage(String msg){
+        byte[] messageBodyBytes = msg.getBytes();
+        try {
+            mChannel.basicPublish(EXCHANGE_NAME, "", null, messageBodyBytes);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
